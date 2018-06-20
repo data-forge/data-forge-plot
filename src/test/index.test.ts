cdc6b7bf7a9d4a1bcc0802909b54f51bfc604eb5
Ashley Davis <ashley@codecapers.com.au>
2018-06-20 17:19:39 +1000

    Fleshed out a real unit test.

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -5,11 +5,46 @@ import "../index";
 
 describe('data-forge-plot', () => {
 
-    it('plot test', ()  => {
+    it('plot series with no configuration', ()  => {
 
         const series = new Series({ index: [1, 2, 3], values: [10, 20, 30] });
         const plotAPI = series.plot();
-        plotAPI.renderImage("some-image.png");
+        
+        expect(plotAPI.serialize()).to.eql({
+            "data": {                      
+                "columnOrder": [           
+                    "__value__",           
+                    "__index__"            
+                ],                         
+                "columns": {               
+                    "__value__": "number", 
+                    "__index__": "number"  
+                },                         
+                "values": [                
+                    {                      
+                        "__value__": 10,   
+                        "__index__": 1     
+                    },                     
+                    {                      
+                        "__value__": 20,   
+                        "__index__": 2     
+                    },                     
+                    {                      
+                        "__value__": 30,   
+                        "__index__": 3     
+                    }                      
+                ]                          
+            },                             
+            "plotDef": {                   
+                "chartType": "line"        
+            },                             
+            "axisMap": {                   
+                "x": "__index__",          
+                "y": [                     
+                    "__value__"            
+                ]                            
+            }
+        });
     });
 
 });
