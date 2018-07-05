3240a6a80ad63265b1ed8af992264bdd0e1bd31c
Ashley Davis <ashley@codecapers.com.au>
2018-07-05 19:12:11 +1000

    Fixes to examples script.

--- examples/run.bat
+++ examples/run.bat
@@ -1,10 +1,10 @@
-ts-node example-1.ts
-ts-node example-2.ts
-ts-node example-3.ts
-ts-node example-4.ts
-ts-node example-5.ts
-ts-node example-6.ts
-ts-node example-7.ts
-ts-node example-8.ts
-ts-node example-9.ts
-ts-node example-10.ts
+call ts-node example-1.ts
+call ts-node example-2.ts
+call ts-node example-3.ts
+call ts-node example-4.ts
+call ts-node example-5.ts
+call ts-node example-6.ts
+call ts-node example-7.ts
+call ts-node example-8.ts
+call ts-node example-9.ts
+call ts-node example-10.ts
