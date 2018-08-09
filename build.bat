8957b61ca02c7085799aa09c5b1650a10a0af4c5
Ashley Davis <ashley@codecapers.com.au>
2018-08-09 20:06:50 +1000

    Restructure templates for new versions of inflate-template and capture-template.

---
+++ build.bat
@@ -0,0 +1,8 @@
+rm -rf build/*
+call npx tsc
+
+call npx browserify ./templates/c3/format-chart-def-src.js -o ./templates/c3/web/assets/format-chart-def.js
+cp ./templates/c3/web/assets/format-chart-def.js ./templates/c3/nodejs/assets/public
+
+call npx browserify ./templates/flot/format-chart-def-src.js -o ./templates/flot/web/assets/format-chart-def.js
+cp ./templates/flot/web/assets/format-chart-def.js ./templates/flot/nodejs/assets/public
